668b1a90f85921f1aa52f0e3bf9b402420a04ef7
CapitanDonD <79704733+CapitanDonD@users.noreply.github.com>
2022-04-22 19:13:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,8 @@ from dotenv import load_dotenv
 from terminaltables import AsciiTable
 
 
-TEMPLATE_TABLE = [
-    ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']
-]
 
-LANGUAGES_FOR_FILL = {
+FOUND_LANGUAGES = {
     'Python': '',
     'Java': '',
     'JavaScript': '',
@@ -20,6 +17,22 @@ LANGUAGES_FOR_FILL = {
 }
 
 
+def files_the_environment():
+    key = os.getenv('SJ_KEY')
+    return key
+
+
+def creating_table(content, title):
+    template = [
+        ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']
+    ]
+    for languages, value in content.items():
+        argument_for_append = [languages,  value['vacancies_found'], value['vacancies_processed'], value['average_salary']]
+        template.append(argument_for_append)
+    table = AsciiTable(template, title)
+    return table.table
+
+
 def predict_rub_salary(of, to):
     if of and to:
         return (of + to)/2
@@ -29,11 +42,12 @@ def predict_rub_salary(of, to):
         return to * 0.8
 
 
-def predict_rub_salary_hhru(found_languages, template):
+def predict_rub_salary_hhru():
     all_average_salaries = []
     url = 'https://api.hh.ru/vacancies'
+    title_hhru = 'HeadHunter Moscow'
 
-    for vacancies in found_languages:
+    for vacancies in FOUND_LANGUAGES:
         params = {
             'period': 30,
             'area': '1',
@@ -42,6 +56,7 @@ def predict_rub_salary_hhru(found_languages, template):
             'per_page': 100,
         }
         response = requests.get(url, params=params)
+        response.raise_for_status()
         vacancies_processed = 0
 
         for i in range(0, response.json()['pages']):
@@ -54,65 +69,60 @@ def predict_rub_salary_hhru(found_languages, template):
                 'per_page': 100,
             }
             response = requests.get(url, params=params)
-            for salary in response.json()['items']:
+            decoded_response = response.json()
+            for salary in decoded_response['items']:
                 if salary['salary'] and salary['salary']['currency'] == 'RUR':
                     all_average_salaries.append(predict_rub_salary(salary['salary']['from'], salary['salary']['to']))
                     vacancies_processed = vacancies_processed + 1
 
-            found_languages[vacancies] = {
+            FOUND_LANGUAGES[vacancies] = {
                 "average_salary": int(sum(all_average_salaries) / len(all_average_salaries)),
-                "vacancies_found": response.json()['found'],
+                "vacancies_found": decoded_response['found'],
                 'vacancies_processed': vacancies_processed
             }
-    title_hhru = 'HeadHunter Moscow'
-
-    for languages, value in found_languages.items():
-        argument_for_append = [languages,  value['vacancies_found'], value['vacancies_processed'], value['average_salary']]
-        template.append(argument_for_append)
-    table = AsciiTable(template, title_hhru)
-    return table.table
+    return creating_table(FOUND_LANGUAGES, title_hhru)
 
 
-def predict_rub_salary_sj(found_languages, template):
+
+
+def predict_rub_salary_sj():
     url = 'https://api.superjob.ru/2.0/vacancies/'
     all_average_salaries = []
-    headers = {'X-Api-App-Id': f'{os.getenv("SECRET_KEY")}'}
-    title = 'SuperJob Moscow'
+    headers = {'X-Api-App-Id': f'{files_the_environment()}'}
+    title_sj = 'SuperJob Moscow'
 
-    for vacancies in found_languages:
-        for i in range(0, 6):
+    for vacancies in FOUND_LANGUAGES:
+        for number_of_page in range(0, 6):
             vacancies_processed = 0
             params = {
                 'period': 30,
                 'catalogues': 48,
-                'page': i,
+                'page': number_of_page,
                 'count': 100,
                 'town': '4',
                 'keyword': f'{vacancies}'
             }
             response = requests.get(url, headers=headers, params=params)
+            response.raise_for_status()
+            decoded_response = response.json()
 
-            for salary in response.json()['objects']:
+            for salary in decoded_response['objects']:
                 if salary['payment_from'] or salary['payment_to'] !=0 or None:
                     if salary['currency'] == 'rub':
                         all_average_salaries.append(predict_rub_salary(salary['payment_from'], salary['payment_to']))
                         vacancies_processed += 1
 
-            found_languages[vacancies] = {
+            FOUND_LANGUAGES[vacancies] = {
                 "average_salary": int(sum(all_average_salaries) / len(all_average_salaries)),
-                "vacancies_found": response.json()['total'],
+                "vacancies_found": decoded_response['total'],
                 'vacancies_processed': vacancies_processed
             }
 
-    for languages, value in found_languages.items():
-        argument_for_append = [languages,  value['vacancies_found'], value['vacancies_processed'], value['average_salary']]
-        template.append(argument_for_append)
-    table = AsciiTable(template, title)
-    return table.table
+    return creating_table(FOUND_LANGUAGES, title_sj)
 
 if __name__ == '__main__':
 
     load_dotenv()
 
-    print(predict_rub_salary_sj(LANGUAGES_FOR_FILL, TEMPLATE_TABLE))
-    print(predict_rub_salary_hhru(LANGUAGES_FOR_FILL, TEMPLATE_TABLE))
+    print(predict_rub_salary_sj())
+    print(predict_rub_salary_hhru())
